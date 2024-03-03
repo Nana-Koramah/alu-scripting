b098da87ddf53ef62fb34d2a18468c8f57b565d1
Nana-Koramah <n.abeasi@alustudent.com>
2024-03-03 16:53:32 +0000

    update

--- api_advanced/0-subs.py
+++ api_advanced/0-subs.py
@@ -2,6 +2,22 @@
 import requests
 
 def number_of_subscribers(subreddit):
+    """
+    Queries the Reddit API and returns the number of subscribers for a given subreddit.
+
+    Args:
+        subreddit (str): The name of the subreddit to query.
+
+    Returns:
+        int: The number of subscribers of the subreddit. Returns 0 if the subreddit is invalid.
+
+    Note:
+        Ensure a valid subreddit name is provided. Invalid subreddits may return 0 subscribers.
+
+    Example:
+        >>> number_of_subscribers('learnpython')
+        1600000
+    """
     url = f"https://www.reddit.com/r/{subreddit}/about.json"
     headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3'}
     response = requests.get(url, headers=headers)
